e41b25d92559d0a0eae732d0f7ce0f135867adb3
Dalton Cole <dalton_cl@yahoo.com>
2017-09-11 19:51:32 -0500

    Commented driver, edited config to specified defaults

--- driver.py
+++ driver.py
@@ -4,19 +4,20 @@
 from helper_functions import *
 
 def main():
+	# If we do not recieve 3 arguments, exit
 	if len(sys.argv) != 3:
 		print('Usage: python3 driver.py <config file> <problem>')
 		quit()
 
-	# {'input_file': '', 'runs': 1, 'random_seed': 1, 'search_algorithm': '', 
-	#  'fitness_evaluations': 1, 'log_file_path': './', 
-	#  'solution_file_path': './', 'algorithm_solution_file_path': './'}
+	"""Dictionary from the config file in the following format:
+		{'input_file': '', 'runs': 1, 'random_seed': 1, 'search_algorithm': '', 
+	  	 'fitness_evaluations': 1, 'log_file_path': './', 
+	  	 'solution_file_path': './', 'algorithm_solution_file_path': './'}
+	"""
 	config_dict = config_parser(sys.argv[1])
 
 	# Set problem file
 	config_dict['input_file'] = sys.argv[2]
-
-	#random.seed(config_dict['random_seed'])
 
 	# Create log file
 	create_log_file(config_dict)
@@ -34,6 +35,7 @@ def main():
 	signal.signal(signal.SIGINT, signal_handler)
 	# signal.signal(signal.SIGINT, partial(signal_handler, return_dict, config_dict)) # With partial
 
+	# Get max_height and list of shape strings from input file
 	max_height = 0
 	shapes = []
 	with open(config_dict['input_file'], 'r') as f:
@@ -43,12 +45,14 @@ def main():
 			shapes.append(line)
 
 	
-	# Start run
+	# Start each run as different processes. 
+	# Last run should display the progress bar.
 	for run in range(config_dict['runs']):
 		p = multiprocessing.Process(target=run_algorithm, args=(config_dict, max_height, shapes, population_size, run, return_dict))
 		jobs.append(p)
 		p.start()
 
+	# Wait for jobs to finish
 	for proc in jobs:
 		proc.join()
 
